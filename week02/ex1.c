ceb0382e63100e1684f7b9c29d26fab3676eafe0
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2023-09-18 13:37:51 +0300

    Update ex1.c

--- week02/ex1.c
+++ week02/ex1.c
@@ -9,9 +9,9 @@ int main() {
     float d = FLT_MAX;
     double e = DBL_MAX;
     
-    printf("%d %d\n", sizeof(a), a); 
-    printf("%d %d\n", sizeof(b), b); 
-    printf("%d %ld\n", sizeof(c), c); 
-    printf("%d %f\n", sizeof(d), d); 
-    printf("%d %lf\n", sizeof(e), e); 
+    printf("%ld %d\n", sizeof(a), a); 
+    printf("%ld %d\n", sizeof(b), b); 
+    printf("%ld %ld\n", sizeof(c), c); 
+    printf("%ld %f\n", sizeof(d), d); 
+    printf("%ld %lf\n", sizeof(e), e); 
 }
