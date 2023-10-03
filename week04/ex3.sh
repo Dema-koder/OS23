fd539f0f87c2e2bb6b6f253d83bf9f0ebf15d12d
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2023-10-03 23:25:34 +0300

    Update ex3.sh

--- week04/ex3.sh
+++ week04/ex3.sh
@@ -1,8 +1,20 @@
 #!/bin/bash 
 gcc ex3.c -o ex3
 ./ex3 3 &
-sleep 1
-pstree -p > ex3_1.txt
+last_program=$!
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
 ./ex3 5 &
-sleep 1
-pstree -p > ex3_2.txt
+last_program=$!
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
+sleep 5
+pstree -p $last_program
